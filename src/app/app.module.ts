f2fddce6244e228b2c304c51f4c17f851a53bed8
Josh Barthelmess <joshbarthelmess3@gmail.com>
2024-01-22 11:38:31 -0500

    fix module stuff

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  daysToChristmas = 0;
 
+  ngOnInit() {
+    this.daysToChristmas = this.findDaysToChristmas();
+  }
+    
+
+  findDaysToChristmas() {
+    const dayOfYear = (date: Date) => Math.floor((date.getTime() - new Date(date.getFullYear(), 0, 0).getTime()) / 1000 / 60 / 60 / 24)
+    let today = new Date();
+    let christmas = new Date(`12/25/${today.getFullYear()}`);
+    return dayOfYear(christmas) - dayOfYear(today);
+  }
 }
