0f842fffe6fc3c22de1eff11ee2b8ea87b9a75dc
Liam Frager <liam.frager@gmail.com>
2024-01-31 14:07:47 -0500

    added html/css for all routes. needs functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+// Login
 import { LoginComponent } from './routes/login/login.component';
+// Wish List
 import { WishListComponent } from './routes/wish-list/wish-list.component';
+import { AddGiftComponent } from './routes/wish-list/add-gift/add-gift.component';
+// Family
 import { FamilyComponent } from './routes/family/family.component';
+import { AddFamilyComponent } from './routes/family/add-family/add-family.component';
+// Shopping List
 import { ShoppingListComponent } from './routes/shopping-list/shopping-list.component';
+// Secret Santa
 import { SecretSantaComponent } from './routes/secret-santa/secret-santa.component';
+// Settings
 import { SettingsComponent } from './routes/settings/settings.component';
 
 export const routes: Routes = [
@@ -16,8 +24,16 @@ export const routes: Routes = [
     component: WishListComponent,
   },
   {
+    path: 'wish-list/add-gift',
+    component: AddGiftComponent,
+  },
+  {
     path: 'family',
     component: FamilyComponent,
+  },
+  {
+    path: 'family/add-family',
+    component: AddFamilyComponent,
   },
   {
     path: 'shopping-list',
---
+++ src/app/routes/wish-list/add-gift/add-gift.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-gift',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-gift.component.html',
+  styleUrl: './add-gift.component.css'
+})
+export class AddGiftComponent {
+
+}
