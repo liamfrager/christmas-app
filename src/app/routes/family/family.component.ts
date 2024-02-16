b0c37f9309e3b2666c064096a3db6fbf6e61a4eb
Liam Frager <liam.frager@gmail.com>
2024-02-15 22:58:07 -0500

    simplified bubble to just take uid; added display for family members

--- src/app/routes/family/family.component.ts
+++ src/app/routes/family/family.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FamilyDisplayComponent } from '../../components/family-display/family-display.component';
 
 @Component({
   selector: 'app-family',
   standalone: true,
-  imports: [],
+  imports: [FamilyDisplayComponent],
   templateUrl: './family.component.html',
   styleUrl: './family.component.css'
 })
