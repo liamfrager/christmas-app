18012ebd8033a1664d6773551381fefe37670dd6
Liam Frager <130017622+liamfrager@users.noreply.github.com>
2024-07-09 14:46:36 -0400

    refactor list-display.component (#38)
    * refactor List data type and list-display.component

--- src/app/routes/family/list/list.component.ts
+++ src/app/routes/family/list/list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ListDisplayComponent } from '../../../components/list-components/list-display/list-display.component';
+import { List } from '../../../types';
+import { GiftListService } from '../../../services/gift-list.service';
 
 @Component({
   selector: 'app-family-list',
@@ -10,11 +12,14 @@ import { ListDisplayComponent } from '../../../components/list-components/list-d
   styleUrl: './list.component.css'
 })
 export class FamilyListComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
-  uid: string = "";
+  constructor(private giftListService: GiftListService, private route: ActivatedRoute) {}
+  listInfo!: List;
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.uid = params['uid'];
+    this.route.queryParams.subscribe(async params => {
+      const listInfo = await this.giftListService.getWishListInfo(params['uid']);
+      if (listInfo) {
+        this.listInfo = listInfo;
+      }
     });
   }
 }
