8e6a147542ac6401dd7160a80d2cb1ada7e7f85a
Liam Frager <liam.frager@gmail.com>
2024-07-14 23:47:59 -0400

    rework gift-list.service db write commands to utilize transactions

--- src/app/services/gift-list.service.ts
+++ src/app/services/gift-list.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { addDoc, collection, doc, getDoc, getDocs, orderBy, query, setDoc, updateDoc } from 'firebase/firestore';
+import { addDoc, collection, doc, getDoc, getDocs, orderBy, query, runTransaction, setDoc, updateDoc } from 'firebase/firestore';
 import { FirebaseService } from './firebase.service';
 import { AccountService } from './account.service';
 import { Gift, List, NewGift, User } from '../types';
@@ -70,22 +70,26 @@ export class GiftListService {
   async addGiftToWishList(gift: NewGift) {
     const currentUserID = await this.accountService.getCurrentUserID();
     if (currentUserID) {
-      // update db.gifts
-      const giftRef = await addDoc(collection(this.firebaseService.db, 'gifts'), {
-        name: gift.name,
-        url: gift.url,
-        details: gift.details,
-        isWishedBy: currentUserID,
-      });
+      await runTransaction(this.db, async (transaction) => {
+        // update db.gifts
+        const giftRef = doc(collection(this.db, 'gifts'))
+        transaction.set(giftRef, {
+          name: gift.name,
+          url: gift.url,
+          details: gift.details,
+          isWishedBy: currentUserID,
+        });
 
-      // update current user's wish-list
-      const wishRef = await updateDoc(doc(this.firebaseService.db, 'lists', currentUserID, 'wish-list', giftRef.id), {
-        id: giftRef.id,
-        name: gift.name,
-        url: gift.url,
-        details: gift.details,
-        isWishedBy: currentUserID,
-      });
+        // update current user's wish-list
+        const wishRef = doc(this.db, 'lists', currentUserID, 'wish-list', giftRef.id)
+        transaction.set(wishRef, {
+          id: giftRef.id,
+          name: gift.name,
+          url: gift.url,
+          details: gift.details,
+          isWishedBy: currentUserID,
+        });
+      })
     }
   }
 
@@ -100,27 +104,29 @@ export class GiftListService {
     } else {
       const currentUserID = await this.accountService.getCurrentUserID();
       if (currentUserID) {
-        // update db.gifts
-        const giftRef = doc(this.firebaseService.db, 'gifts', gift.id)
-        updateDoc(giftRef, {
-          isClaimedBy: currentUserID,
-          status: 'claimed',
-        })
+        runTransaction(this.db, async (transaction) => {
+          // update db.gifts
+          const giftRef = doc(this.db, 'gifts', gift.id)
+          transaction.update(giftRef, {
+            isClaimedBy: currentUserID,
+            status: 'claimed',
+          });
 
-        // update current user's shopping-list
-        const shoppingRef = doc(this.firebaseService.db, 'lists', currentUserID, 'shopping-list', gift.id);
-        setDoc(shoppingRef, {
-          ...gift,
-          isWishedBy: await this.accountService.getUserInfo(currentUserID),
-          status: 'claimed',
+          // update current user's shopping-list
+          const shoppingRef = doc(this.db, 'lists', currentUserID, 'shopping-list', gift.id);
+          transaction.set(shoppingRef, {
+            ...gift,
+            isWishedBy: await this.accountService.getUserInfo(currentUserID),
+            status: 'claimed',
+          });
+
+          // update isWishedBy user's wish-list
+          const wishedByID = typeof gift.isWishedByID;
+          const wishRef = doc(this.db, 'lists', wishedByID, 'wish-list', gift.id);
+          transaction.update(wishRef, {
+            status: 'claimed',
+          });
         });
-
-        // update isWishedBy user's wish-list
-        const wishedByID = typeof gift.isWishedByID;
-        const wishRef = doc(this.firebaseService.db, 'lists', wishedByID, 'wish-list', gift.id);
-        updateDoc(wishRef, {
-          status: 'claimed',
-        })
       } 
     }
   }
