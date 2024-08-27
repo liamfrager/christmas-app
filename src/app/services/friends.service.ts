44aafebc96886dc33c8b259aa9b3120c73a05886
Liam Frager <liam.frager@gmail.com>
2024-08-27 18:04:49 -0400

    add deleteFriend function, fix bug in addFriend function

--- src/app/services/friends.service.ts
+++ src/app/services/friends.service.ts
@@ -33,7 +33,7 @@ export class FriendsService {
           status: 'outgoing',
         });
 
-        const newFriendRequestRef = doc(collection(this.db, "lists", this.currentUser['uid'], "friends-list"));
+        const newFriendRequestRef = doc(collection(this.db, "lists", newFriendData.id, "friends-list"));
         transaction.set(newFriendRequestRef, {
           ...this.currentUser,
           status: 'incoming',
@@ -42,6 +42,20 @@ export class FriendsService {
       console.log("Friend successfully added");
     } catch (e) {
       console.error("Error adding friend: ", e);
+    }
+  }
+    
+  async deleteFriend(friend: User) {
+    try {
+      await runTransaction(this.db, async (transaction) => {
+        const friendRef = doc(this.db, "lists", this.currentUser['uid'], "friends-list", friend.id);
+        transaction.delete(friendRef);
+        const userAsFriendRef = doc(this.db, "lists", friend.id, "friends-list", this.currentUser['uid']);
+        transaction.delete(userAsFriendRef);
+      });
+      console.log("Friend successfully removed");
+    } catch (e) {
+      console.error("Error removing friend: ", e);
     }
   }
 }
