efdcd67e2c4934ed0f671b325b46c28b263e061f
Liam Frager <liam.frager@gmail.com>
2024-09-01 22:42:07 -0400

    create and handle add-shopping-gift form

--- src/app/routes/shopping-list/add-gift/add-shopping-gift.component.ts
+++ src/app/routes/shopping-list/add-gift/add-shopping-gift.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GiftListService } from '../../../services/gift-list.service';
+import { AccountService } from '../../../services/account.service';
+import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Friend, NewGift } from '../../../types';
+import { CommonModule } from '@angular/common';
+import { FriendsService } from '../../../services/friends.service';
 
 @Component({
   selector: 'app-add-shopping-gift',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-shopping-gift.component.html',
   styleUrl: './add-shopping-gift.component.css'
 })
-export class AddShoppingGiftComponent {
+export class AddShoppingGiftComponent implements OnInit {
+  constructor(
+    private giftListService: GiftListService, 
+    private accountService: AccountService,
+    private friendsService: FriendsService,
+    private router: Router
+  ) {}
+  
+  friends!: Array<Friend>
+  
+  async ngOnInit() {
+    this.friends = await this.friendsService.getFriends()
+  }
 
+  onSubmit(form: NgForm) {
+    const gift: NewGift = {
+      name: form.form.value.name,
+      url: form.form.value.url,
+      details: form.form.value.details,
+      isWishedByID: form.form.value.friend.id,
+    }
+    const friend = form.form.value.friend
+    this.giftListService.createGiftInShoppingList(gift, friend)
+    this.router.navigate(['./shopping-list'])
+  }
 }
