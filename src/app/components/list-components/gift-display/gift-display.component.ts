6b4c6cc9c94789663b00e4933b609577c1021beb
Liam Frager <liam.frager@gmail.com>
2024-09-05 20:48:07 -0400

    add checkboxTitle tooltip to indicate gift has been deleted

--- src/app/components/list-components/gift-display/gift-display.component.ts
+++ src/app/components/list-components/gift-display/gift-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Gift } from '../../../types';
 
 @Component({
   selector: 'app-gift-display',
@@ -7,10 +8,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './gift-display.component.html',
   styleUrl: './gift-display.component.css'
 })
-export class GiftDisplayComponent {
-  @Input({required: true}) gift!: any;
+export class GiftDisplayComponent implements OnChanges {
+  @Input({required: true}) gift!: Gift;
   @Input({required: true}) checkType!: 'circle' | 'check_circle' | 'error';
-
   @Output() giftClicked = new EventEmitter();
+
+  checkboxTitle?: string;
+
+  ngOnChanges(): void {
+    console.log(this.gift);
+    this.checkboxTitle = this.checkType === 'error' ? `This gift has been deleted by ${this.gift.isWishedByUser?.displayName}. It is no longer on their wish list.` : '';
+  }
 
 }
