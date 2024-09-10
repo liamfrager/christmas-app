2603be0dfd73715257e88b769b69f2fb7055df1b
Liam Frager <liam.frager@gmail.com>
2024-09-10 09:44:37 -0400

    generate gift-form component

---
+++ src/app/components/forms/gift-form/gift-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gift-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './gift-form.component.html',
+  styleUrl: './gift-form.component.css'
+})
+export class GiftFormComponent {
+
+}
