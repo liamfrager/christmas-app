b9b710294a832afd7d316f5904c5c94686421cdd
Liam Frager <liam.frager@gmail.com>
2024-09-10 10:11:31 -0400

    replace h2 w/ app-page-heading

--- src/app/routes/shopping-list/add-gift/add-shopping-gift.component.ts
+++ src/app/routes/shopping-list/add-gift/add-shopping-gift.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GiftFormComponent } from "../../../components/forms/gift-form/gift-form.component";
+import { PageHeadingComponent } from "../../../components/page-heading/page-heading.component";
 
 @Component({
   selector: 'app-add-shopping-gift',
   standalone: true,
-  imports: [GiftFormComponent],
+  imports: [GiftFormComponent, PageHeadingComponent],
   templateUrl: './add-shopping-gift.component.html',
   styleUrl: './add-shopping-gift.component.css'
 })
--- src/app/routes/wish-list/add-gift/add-wish-gift.component.ts
+++ src/app/routes/wish-list/add-gift/add-wish-gift.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { GiftFormComponent } from "../../../components/forms/gift-form/gift-form.component";
+import { PageHeadingComponent } from "../../../components/page-heading/page-heading.component";
 
 
 @Component({
   selector: 'app-add-wish-gift',
   standalone: true,
-  imports: [GiftFormComponent],
+  imports: [GiftFormComponent, PageHeadingComponent],
   templateUrl: './add-wish-gift.component.html',
   styleUrl: './add-wish-gift.component.css'
 })
