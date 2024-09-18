8fc28d5ca8fe87e885efeba8e1fd936270a4992d
Liam Frager <liam.frager@gmail.com>
2024-09-17 22:05:25 -0400

    allow gift-form to accept gift input for default form values

--- src/app/components/forms/gift-form/gift-form.component.ts
+++ src/app/components/forms/gift-form/gift-form.component.ts
@@ -3,7 +3,7 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { GiftListService } from '../../../services/gift-list.service';
 import { FriendsService } from '../../../services/friends.service';
 import { Router } from '@angular/router';
-import { Friend, NewGift } from '../../../types';
+import { Friend, Gift, NewGift } from '../../../types';
 import { PageHeadingComponent } from "../../page-heading/page-heading.component";
 import { PfpSelectComponent } from '../../pfp-select/pfp-select.component';
 import { CommonModule } from '@angular/common';
@@ -22,11 +22,22 @@ export class GiftFormComponent {
     private router: Router
   ) {}
   @Input({required: true}) type!: 'wish' | 'shopping';
+  @Input() gift?: Gift;
   
   friends: Array<Friend> = [];
   selectedFriend?: Friend;
   
+  // Form values
+  nameVal?: string;
+  urlVal?: string;
+  detailsVal?: string;
+
+
+  
   async ngOnInit() {
+    this.nameVal = this.gift ? this.gift.name : '';
+    this.urlVal = this.gift ? this.gift.url : '';
+    this.detailsVal = this.gift ? this.gift.details : '';
     if (this.type === 'shopping') {
       this.friends = await this.friendsService.getFriends()
     }
