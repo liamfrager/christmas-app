0d71920757f21187543675c62a3d102c29fb505f
Liam Frager <liam.frager@gmail.com>
2024-09-27 00:05:40 -0400

    hide footer when not logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  constructor(public authService: AuthService) {}
   title = 'christmas-app';
 }
