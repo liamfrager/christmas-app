b6e158b0e450884b3e6c4aa15950fdfce045bc61
Liam Frager <liam.frager@gmail.com>
2024-10-01 13:54:43 -0400

    add filler component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/ui/header/header.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './components/ui/footer/footer.component';
 
 @NgModule({
   declarations: [],
