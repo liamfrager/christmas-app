49d8487f98f5f54f8a1c4776b676fa3145cc3ecc
Liam Frager <liam.frager@gmail.com>
2024-10-01 20:46:51 -0400

    disable click animation on functionless icons

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -1,20 +1,16 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-icon',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './icon.component.html',
   styleUrl: './icon.component.css'
 })
-export class IconComponent implements OnChanges {
+export class IconComponent {
   @Input({required: true}) icon!: string;
   @Input() hover: boolean = true;
   @Input() size: string = '30px'; // Default size.
   @Output() iconClicked = new EventEmitter();
-
-  style = "material-symbols-outlined icon"
-  ngOnChanges() {
-    this.style = this.hover ? "material-symbols-outlined icon" : "material-symbols-outlined";
-  }
 }
