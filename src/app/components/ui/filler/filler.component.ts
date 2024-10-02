4bd6a9e2a55cdb7a78c72a7f59c9e2efbf4bbf15
Liam Frager <liam.frager@gmail.com>
2024-10-01 23:27:42 -0400

    add dynamic footer-filler size

--- src/app/components/ui/filler/filler.component.ts
+++ src/app/components/ui/filler/filler.component.ts
@@ -11,9 +11,10 @@ export class FillerComponent implements OnChanges {
   @Input() size: string = '1em';
 
   ngOnChanges() {
+    const isPWA = window.matchMedia('(display-mode: standalone)').matches;
     if (this.size === 'header')
       this.size = '8em';
     else if (this.size === 'footer')
-      this.size = '4em';
+      this.size = isPWA ? '5.5em' : '4em';
   }
 }
