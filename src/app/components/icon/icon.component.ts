10fb5473b5b1128530966eb853571eb8a3ed9cb7
Liam Frager <liam.frager@gmail.com>
2024-10-01 23:49:46 -0400

    add hover === null to icon; fix status update line

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -1,6 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+/**
+   * Fetches the wish-list of a given user.
+   * @Input icon - The name of the [Google Icon](https://fonts.google.com/icons) to be displayed.
+   * @Input hover - An indication whether or not the icon should be clickable. Defaults to `true`. Will not grow on hover but can still be clicked on `false`. Clicking is disabled on `null`.
+   * @Input size - The font-size of the icon as a string. Defaults to `30px`.
+   * @Output iconClicked - Event emitter for when the icon is clicked. Will not emit if `hover == null`.
+   */
 @Component({
   selector: 'app-icon',
   standalone: true,
@@ -10,7 +17,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class IconComponent {
   @Input({required: true}) icon!: string;
-  @Input() hover: boolean = true;
+  @Input() hover: boolean | null = true;
   @Input() size: string = '30px'; // Default size.
   @Output() iconClicked = new EventEmitter();
 }
