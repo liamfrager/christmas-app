250dde2c9dd7870f021106d220059299e41c2c60
Liam Frager <liam.frager@gmail.com>
2024-10-04 22:15:42 -0400

    adjust theme color on header toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/ui/header/header.component';
 import { FooterComponent } from './components/ui/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { FirebaseService } from './services/firebase.service';
 import { FillerComponent } from "./components/ui/filler/filler.component";
-import { Settings } from './types';
 import { SettingsService } from './services/settings.service';
 
 @Component({
@@ -15,9 +14,22 @@ import { SettingsService } from './services/settings.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  constructor(public firebaseService: FirebaseService, private settingsService: SettingsService) {}
+export class AppComponent implements OnInit {
+  constructor(public firebaseService: FirebaseService, private settingsService: SettingsService, private renderer: Renderer2) {}
   title = 'christmas-app';
   isLoggedIn = localStorage.getItem('isLoggedIn');
   settings = this.settingsService.settings;
+
+  ngOnInit() {
+    console.log('changing color')
+    const themeMetaTag = document.querySelector('meta[name="theme-color"]');
+    if (themeMetaTag) {
+      if (this.settings.showHeader){
+        this.renderer.setAttribute(themeMetaTag, 'content', '#d64541');
+      } else {
+        this.renderer.setAttribute(themeMetaTag, 'content', 'cornsilk');
+      }
+    }
+
+  }
 }
