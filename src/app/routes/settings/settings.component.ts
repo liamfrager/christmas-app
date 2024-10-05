eecdcfc69dfc0dcfe622a82808ea92456fe1b2e9
Liam Frager <liam.frager@gmail.com>
2024-10-04 22:56:29 -0400

    update profile button icon hover

--- src/app/routes/settings/settings.component.ts
+++ src/app/routes/settings/settings.component.ts
@@ -3,13 +3,14 @@ import { AuthService } from '../../services/auth.service';
 import { PageHeadingComponent } from "../../components/page-heading/page-heading.component";
 import { Settings } from '../../types';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { SettingsService } from '../../services/settings.service';
+import { IconComponent } from "../../components/icon/icon.component";
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule, FormsModule, PageHeadingComponent],
+  imports: [CommonModule, FormsModule, PageHeadingComponent, IconComponent],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
@@ -20,12 +21,11 @@ export class SettingsComponent {
   ) {}
   settings: Settings = this.settingsService.settings;
 
-  onSettingsChange(setting: string, value: any) {
-    this.settingsService.updateSettings({[setting]: value});
+  onSettingsChange(form: NgForm) {
+    this.settingsService.updateSettings(form.form.value);
   }
 
   logoutUser() {
     this.authService.logoutUser();
   }
-
 }
