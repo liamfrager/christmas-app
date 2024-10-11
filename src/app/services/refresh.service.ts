b19966a9ba75060069cce9093d9114ddf636be82
Liam Frager <liam.frager@gmail.com>
2024-10-11 09:51:26 -0400

    onRefresh() calls function once in ngOnInit; add documentation

--- src/app/services/refresh.service.ts
+++ src/app/services/refresh.service.ts
@@ -6,7 +6,15 @@ export class RefreshService {
   public static swipeStartY: number = 0;
   private static callbackMap = new Map<any, (() => void)[]>();
 
-  static onRefresh() {
+  /**
+   * Decorator for functions that will be called whenever a user swipes down at the top of the page to refresh.
+   * Only works when being used as a Progressive Web App (PWA).
+   * Methods decorated with `@RefreshService.onRefresh()` will be run once at the end of the component's `OnInit` lifecycle.
+   * The methods will no longer be called on refresh at the end of the component's `OnDestroy` lifecycle.
+   * 
+   * All methods of currently instantiated components decorated with `@RefreshService.onRefresh()` can be called manually by `@RefreshService.triggerRefresh()`.
+   */
+  public static onRefresh() {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
       console.log('target', target)
       const originalMethod = descriptor.value;
@@ -16,15 +24,14 @@ export class RefreshService {
           originalNgOnInit.apply(this, args);
         }
         let callbacks = [() => {
-          setTimeout(() => {
-            originalMethod.apply(this, args);
-          });
+          originalMethod.apply(this, args);
         }]
         const index = this;
         if (RefreshService.callbackMap.has(index)) {
           callbacks = [...callbacks, ...RefreshService.callbackMap.get(index)!];
         }
         RefreshService.callbackMap.set(index, callbacks);
+        originalMethod.apply(this, args);
       };
       const originalNgOnDestroy = target.ngOnDestroy;
       target.ngOnDestroy = function (...args: any[]) {
@@ -37,7 +44,10 @@ export class RefreshService {
     };
   }
 
-  static triggerRefresh() {
+  /**
+   * Calls all methods of currently instantiated components decorated with the `@RefreshService.onRefresh()` decorator.
+   */
+  public static triggerRefresh() {
     console.log(RefreshService.callbackMap)
     for (let key of RefreshService.callbackMap.keys()) {
       const callbacks = this.callbackMap.get(key);
@@ -45,7 +55,7 @@ export class RefreshService {
     }
   }
 
-  static clearCallbacks(key: any) {
+  public static clearCallbacks(key: any) {
     if (RefreshService.callbackMap.has(key))
       RefreshService.callbackMap.delete(key);
   }
