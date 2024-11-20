3f558087446ae780ee8dfa302e5075035ff56254
Liam Frager <liam.frager@gmail.com>
2024-11-19 19:31:56 -0500

    add direct gift navigation in friends list

--- src/app/components/user-display/user-display.component.ts
+++ src/app/components/user-display/user-display.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { User } from '../../types';
 import { Router } from '@angular/router';
+import { IconComponent } from "../icon/icon.component";
 
 @Component({
   selector: 'app-user-display',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IconComponent],
   templateUrl: './user-display.component.html',
   styleUrl: './user-display.component.css'
 })
@@ -14,12 +15,20 @@ export class UserDisplayComponent {
   constructor(private router: Router) {};
   @Input({required: true}) user!: User;
   @Input() onClick: (() => void) | null = this.goToProfile;
+  @Input() icon: string | null = null;
 
   /**
    * Reroutes the webpage to the displayed user's profile.
    */
   goToProfile() {
-    this.router.navigate([`/profile/${this.user.id}`]);
+    this.router.navigate(['profile', this.user.id]);
+  }
+
+  /**
+   * Reroutes the webpage to the displayed user's wish-lists.
+   */
+  goToLists() {
+    this.router.navigate(['profile', this.user.id, 'wish-lists']);
   }
   
   /**
