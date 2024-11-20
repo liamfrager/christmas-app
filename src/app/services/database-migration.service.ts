65466d82025538d1d4c1d6079206140b27b92ab3
Liam Frager <liam.frager@gmail.com>
2024-11-19 23:40:33 -0500

    add ids to all gifts

--- src/app/services/database-migration.service.ts
+++ src/app/services/database-migration.service.ts
@@ -100,4 +100,49 @@ export class DatabaseMigrationService {
       console.error('Error during migration:', error);
     }
   }
+
+  async addIDToGifts() {
+    try {
+      // Loop through each user
+      const allUsers = ['4s4B1axxXEQteVDp5DtuOq72pK42', 'EQXtBFdyjehT81WTjwxigB30zC52', 'Geim2QD1v6Nm413ICCUpApZFO6z1', 'PEix0xEDyZVwMW4wMfxY83Qbaij2', 'VgLKDGuMzuXi5F0nccEq5Gnlp9l1', 'osUu9SKamWUJK7jM8REk9BZKRTK2', 'ujtlFX3zwPOZHQM15jon9eAqPu43', 'xcLuPEudGKThyy4MKKFYCG2wQrm1', 'zncVQJy3qbUqHdRI6GyJsUrFhV72'];
+      for (const id of allUsers) {
+        const user: User = await this.accountService.getUserInfo(id) as User;
+        console.log(`updating gifts for user with id ${id}: `, user);
+        try {
+          // Reference the old "wish-list" collection
+          const listsSnap = await getDocs(collection(this.db, 'lists', user.id, 'wish-lists'));
+          const lists = listsSnap.docs;
+          for (const list of lists) {
+            const wishList = await getDocs(collection(this.db, 'lists', user.id, 'wish-lists', list.id, 'gifts'));
+            const giftPromises = wishList.docs.map(async (gift) => {
+              try {
+                await updateDoc(gift.ref, {id: gift.id});
+                console.log(`Added ID to gift: ${gift.id}`);
+              } catch (error) {
+                console.error(`Error migrating gift: ${gift.id}`, error);
+              }
+            });
+            await Promise.all(giftPromises);
+            console.log(`Migrated all gifts for list: ${list.id}`)
+          }
+          const shoppingList = await getDocs(collection(this.db, 'lists', user.id, 'shopping-list'));
+          const shoppingPromises = shoppingList.docs.map(async (gift) => {
+            try {
+              await updateDoc(gift.ref, {id: gift.id});
+              console.log(`Added ID to gift: ${gift.id}`);
+            } catch (error) {
+              console.error(`Error migrating gift: ${gift.id}`, error);
+            }
+          });
+          await Promise.all(shoppingPromises);
+          console.log(`Migrated all gifts for user: ${user.id}`)
+        } catch (error) {
+          console.error(`Error migrating wish-list for user: ${user.id}`, error);
+        }
+      }
+      console.log('Migration completed successfully.');
+    } catch (error) {
+      console.error('Error during migration:', error);
+    }
+  }
 }
